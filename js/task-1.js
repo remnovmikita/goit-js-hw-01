9274bdf2af48f9ae35eb6eafd635681439b1e7d0
Nikita <remnovmikita@gmail.com>
2025-11-17 15:44:45 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction (quantity, pricePerDroid){
     let totalPrice = pricePerDroid * quantity;
-    return `You ordered ${quantity} droids ${totalPrice} credits`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits`;
 }
 
 
